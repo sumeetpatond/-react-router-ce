6147a9a198a2c7728c9c04d8f6d475e4c30f964b
sumeet-07 <sumeetpatond@gmail.com>
2022-02-26 01:31:53 +0530

    handled missing route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './componentes/Home';
 import About from './componentes/About';
 import NavBar from './componentes/NavBar';
 import OrderSummary from './componentes/OrderSummary';
+import PageNotFound from './componentes/PageNotFound';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/order-summary" element={<OrderSummary />} />
+                <Route path="*" element={<PageNotFound />} />
             </Routes>
         </>
     );
